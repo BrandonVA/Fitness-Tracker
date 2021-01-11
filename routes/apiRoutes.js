e56add9149dde6d72da924470de8d4fe5ec561d7
BrandonVA <codebva@gmail.com>
2021-01-11 13:18:08 -0800

    Fixed adding exercises when it gets added it now updates the correct workout collection based on its id.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,8 +18,9 @@ module.exports = (app) => {
 
 
     app.put("/api/workouts/:id", (req, res) => {
+        const id = req.params.id
         db.Exercise.create(req.body)
-        .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+        .then(({_id}) => db.Workout.findOneAndUpdate({_id: id}, { $push: { exercises: _id } }, { new: true }))
         .then(dbWorkout => {
           res.json(dbWorkout);
         })
@@ -31,10 +32,10 @@ module.exports = (app) => {
 
 
     app.post("/api/workouts", (req, res) => {
-        console.log(req.params.id)
+        // console.log(req.params.id)
         db.Workout.create(req.body)
             .then(Workout => {
-                console.log(Workout)
+                // console.log(Workout)
                 res.json(Workout);
             })
             .catch(err => {
@@ -42,5 +43,69 @@ module.exports = (app) => {
             });
     });
 
+
+    app.get("/api/workouts/range", (req, res) => {
+        const weekAgo = new Date(new Date() - 7 * 60 * 60 * 24 * 1000);
+        // working query ---> db.workouts.aggregate( [ {$match: { day: { $gte: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) }  } }])
+        
+        
+        /*
+
+
+        
+        db.Workout.aggregate( [ 
+            { $match: 
+                { day: { $gte: weekAgo }  } 
+            },
+            { 
+                $addFields: {
+                  totalWeight: { $sum: "$weight" } ,
+                  totalDuration: { $sum: "$duration" }
+                }
+            }
+        ]).then(Workouts => {
+            console.log(Workouts)
+            res.json(Workouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+        
+        
+        */
+
+            // db.Workout.aggregate( [
+            //     { $match: { $or: [ { day: { $gt: weekAgo, $lt: Date() } } ] } },
+            //     // { $group: { _id: null, count: { $sum: 1 } } }
+            //   ] );
+    })
+
 }
+
+
+// db.workouts.aggregate( [ 
+//     {
+//         $match: {
+//             day: { $gte: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) }  
+//          }
+//      }, 
+     
+
+         
+//     { 
+//         $addFields: {
+//              totalWeight: { $sum: "$exercises.weight" } ,
+//              totalDuration: { $sum: "$exercises.duration" }
+//         }
+//      },
+//      {$lookup:
+//      {
+//        from: "exersices",
+//        localField: "exersices",
+//        foreignField: "_id",
+//        as: "weight"
+//      }
+//  }
+       
+//  ])
 
