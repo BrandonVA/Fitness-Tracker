c88ba2007dfb25c2c241c548715fa456707ebe1e
BrandonVA <codebva@gmail.com>
2021-01-12 15:44:32 -0800

    Added route for getting last 7 exercises.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const db = require("../models");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
+mongoose.set('useFindAndModify', false);
 
 module.exports = (app) => {
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
-        .populate("exercises")
+            .populate("exercises")
             .then(Workout => {
                 res.json(Workout);
             })
@@ -20,22 +21,20 @@ module.exports = (app) => {
     app.put("/api/workouts/:id", (req, res) => {
         const id = req.params.id
         db.Exercise.create(req.body)
-        .then(({_id}) => db.Workout.findOneAndUpdate({_id: id}, { $push: { exercises: _id } }, { new: true }))
-        .then(dbWorkout => {
-          res.json(dbWorkout);
-        })
-        .catch(err => {
-          res.json(err);
-        });
+            .then(({ _id }) => db.Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: _id } }, { new: true }))
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
-    
+
 
 
     app.post("/api/workouts", (req, res) => {
-        // console.log(req.params.id)
         db.Workout.create(req.body)
             .then(Workout => {
-                // console.log(Workout)
                 res.json(Workout);
             })
             .catch(err => {
@@ -45,67 +44,104 @@ module.exports = (app) => {
 
 
     app.get("/api/workouts/range", (req, res) => {
-        const weekAgo = new Date(new Date() - 7 * 60 * 60 * 24 * 1000);
-        // working query ---> db.workouts.aggregate( [ {$match: { day: { $gte: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) }  } }])
-        
-        
-        /*
 
+        db.Workout.aggregate([
 
-        
-        db.Workout.aggregate( [ 
-            { $match: 
-                { day: { $gte: weekAgo }  } 
+            // Sorting and limiting to last 7 entered
+            { $sort: { _id: -1 } },
+            { $limit: 7 },
+            {
+                // Populating the aggregate results with exercises collection.
+                $lookup: {
+                    from: "exercises",    // From the exercises collection
+                    localField: "exercises", // Local field to make the connection
+                    foreignField: "_id",    // using _id to connect the two
+                    as: "exercises"         // setting the value of the populated collections as the same name so it overwrites the previous one.
+                }
             },
-            { 
+            {
+                // Adding new fields for the total of weights and duration.
                 $addFields: {
-                  totalWeight: { $sum: "$weight" } ,
-                  totalDuration: { $sum: "$duration" }
+                    totalWeight: { $sum: "$exercises.weight" },
+                    totalDuration: { $sum: "$exercises.duration" }
                 }
             }
-        ]).then(Workouts => {
-            console.log(Workouts)
-            res.json(Workouts);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-        
-        
-        */
-
-            // db.Workout.aggregate( [
-            //     { $match: { $or: [ { day: { $gt: weekAgo, $lt: Date() } } ] } },
-            //     // { $group: { _id: null, count: { $sum: 1 } } }
-            //   ] );
+        ])
+            .then(Workouts => {
+                res.json(Workouts);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     })
 
 }
 
+// ______________________________________SHOULD WORK???... YES_________________________________________________________________
+//_______________________________Same as "/api/workouts/range" but MORE DESCRIPTIVE NAMES______________________________________
+// Link for reference: https://stackoverflow.com/questions/34967482/lookup-on-objectids-in-an-array
 
-// db.workouts.aggregate( [ 
+
+// Old way but more descriptive of how its working
+// Leaving as a reference incase of my brain breaks again.
+
+
+
+// db.Workout.aggregate([
+
+//     { $sort: { _id: -1 } },
+//     { $limit: 7 },
+
+//     // Unwind the source
+//     { "$unwind": "$exercises" },
+//     // Do the lookup matching
 //     {
-//         $match: {
-//             day: { $gte: new Date(new Date() - 7 * 60 * 60 * 24 * 1000) }  
-//          }
-//      }, 
-     
-
-         
-//     { 
-//         $addFields: {
-//              totalWeight: { $sum: "$exercises.weight" } ,
-//              totalDuration: { $sum: "$exercises.duration" }
+//         "$lookup": {
+//             "from": "exercises",
+//             "localField": "exercises",
+//             "foreignField": "_id",
+//             "as": "exerciseObjects"
 //         }
-//      },
-//      {$lookup:
-//      {
-//        from: "exersices",
-//        localField: "exersices",
-//        foreignField: "_id",
-//        as: "weight"
-//      }
-//  }
-       
-//  ])
+//     },
+//     // Unwind the result arrays ( likely one or none )
+//     { "$unwind": "$exerciseObjects" },
+//     // Group back to arrays
+//     {
+//         "$group": {
+//             "_id": "$_id",
+//             "exercises": { "$push": "$exercises" },
+//             "exerciseObjects": { "$push": "$exerciseObjects" }
+//              "day": {$first: "$day"},
+//         }
+//     },
+//     {
+//         $addFields: {
+//             totalWeight: { $sum: "$exerciseObjects.weight" },
+//             totalDuration: { $sum: "$exerciseObjects.duration" }
+//         }
+//     }
+// ])
+
+// __________________________SAME AS ABOVE __________________________________________________
+// just using the updated version where $unwind isn't needed
+// db.Workout.aggregate([
+
+//     { $sort: { _id: -1 } },
+//     { $limit: 7 },
+
+//     {
+//         $lookup: {
+//             from: "exercises",
+//             localField: "exercises",
+//             foreignField: "_id",
+//             as: "exercises"
+//         }
+//     },
+//     {
+//         $addFields: {
+//             totalWeight: { $sum: "$exercises.weight" },
+//             totalDuration: { $sum: "$exercises.duration" }
+//         }
+//     }
+// ])
 
