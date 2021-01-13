0389f0c094e9b5f4590794557c1b95859d2a3715
BrandonVA <codebva@gmail.com>
2021-01-13 14:45:34 -0800

    Updating MongoDB connection.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-mongoose.set('useFindAndModify', false);
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout_db",
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 module.exports = (app) => {
     app.get("/api/workouts", (req, res) => {
